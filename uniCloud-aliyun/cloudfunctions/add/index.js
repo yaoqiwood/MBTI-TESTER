0a689e51efc57b4910800e4a35556abc95bfd8ed
yaoqiwood <yaoqiwood@gmail.com>
2026-03-19 10:19:19 +0800

    chore: 移除废弃的测试文件和云函数代码
    清理不再使用的测试数据文件、云函数及相关配置，包括：
    1. 删除多个.init_data.json测试数据文件
    2. 移除prohibitedWords、hello-common等云函数
    3. 清理clientDB权限演示相关代码
    4. 删除storage页面和扩展存储示例
    5. 移除uni-admin后台管理相关代码

--- uniCloud-aliyun/cloudfunctions/add/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const db = uniCloud.database()
-exports.main = async (event, context) => {
-	const collection = db.collection('unicloud-test')
-	const { product } = event
-	const res = await collection.add({ product })
-	return res
-};
- 
